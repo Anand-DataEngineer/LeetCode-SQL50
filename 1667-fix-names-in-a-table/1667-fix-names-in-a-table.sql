46771421e015dd13f0adbf357e7f5b127648fdff
Anand-DataEngineer <anandravide@outlook.com>
2026-08-09 12:48:31 +0530

    Time: 1029 ms (6.43%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -2,6 +2,6 @@
 
 select
    user_id,
-   concat(upper(left(name,1)), lower(substring(name,2,length(name)-1))) as name
+   concat(upper(left(name,1)), lower(substring(name,2))) as name
 from users
 order by user_id;
