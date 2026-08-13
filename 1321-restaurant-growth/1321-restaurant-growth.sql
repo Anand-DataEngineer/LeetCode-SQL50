3208645c585bd0f92006114c037ff1dbb61802fe
Anand-DataEngineer <anandravide@outlook.com>
2026-08-13 17:44:32 +0530

    Time: 532 ms (8.13%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,26 +1,13 @@
 # Write your MySQL query statement below
-with min_date_cte as(
-select
-   min(visited_on) as min_date
-from Customer
-),
-day_summed_up_cte as(
-    select
-       visited_on,
-       sum(amount) as amount
-    from Customer
-    group by visited_on
-),
-overall_cte as (
-select
+with overall_cte as (
+select distinct
   visited_on,
-  Round(sum(amount) over(order by visited_on range between interval 6 day preceding and current row),2) as amount,
-  Round(avg(amount) over(order by visited_on range between interval 6 day preceding and current row),2) as average_amount
-from day_summed_up_cte
+  Round(sum(amount) over(order by visited_on range between interval 6 day preceding and current row),2) as amount
+from customer
 )
 select
    visited_on,
    amount,
-   average_amount
+   Round(amount/7,2) as average_amount
 from overall_cte
-where datediff(visited_on,(select min_date from min_date_cte)) >5;
+where datediff(visited_on,(select min(visited_on) from Customer)) >5;
