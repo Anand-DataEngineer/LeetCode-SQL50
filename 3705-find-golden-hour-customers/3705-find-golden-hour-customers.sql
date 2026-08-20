dcf5dacf7a6c5f8988aba1c43d5d10f6cfc9a122
Anand-DataEngineer <anandravide@outlook.com>
2026-08-20 12:41:39 +0530

    Time: 360 ms (29.93%), Space: 0B (100%) - LeetHub

--- 3705-find-golden-hour-customers/3705-find-golden-hour-customers.sql
+++ 3705-find-golden-hour-customers/3705-find-golden-hour-customers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-with rated_cte as(
+
 select
     customer_id,
     count(*) as total_orders,
@@ -8,17 +8,13 @@ select
              then 1 
              else 0
         end)/count(*) * 100 , 0) as peak_hour_percentage,
-    Round(avg(order_rating),2) as average_rating,
-    sum(case when order_rating is NULL then 0 else 1 end)/count(*) * 100.0 as rated_percentage
+    Round(avg(order_rating),2) as average_rating
 from restaurant_orders
-group by customer_id
-)
-select
-   rated_cte.customer_id,
-   rated_cte.total_orders,
-   rated_cte.peak_hour_percentage,
-   rated_cte.average_rating
-from rated_cte
-where total_orders > 2 AND peak_hour_percentage >= 60 AND average_rating >= 4.0 AND rated_percentage >= 50 
-order by average_rating desc, customer_id desc; 
+group by customer_id 
+having total_orders > 2 AND
+       peak_hour_percentage >= 60 AND
+       average_rating >= 4.0 AND
+       count(order_rating)/count(*) >= 0.5
+order by average_rating desc, customer_id desc;
+
    
