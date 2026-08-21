93389805bd23270434944ac0e58d718ee68ab3c2
Anand-DataEngineer <anandravide@outlook.com>
2026-08-21 19:57:52 +0530

    Time: 358 ms (47.24%), Space: 0B (100%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -1,22 +1,9 @@
 # Write your MySQL query statement below
-with paiduser_cte as(
-select 
-    *
-from UserActivity 
-where user_id in
-(select
-   distinct user_id
-from UserActivity
-where activity_type = 'paid'
-intersect
-select
-   distinct user_id
-from UserActivity
-where activity_type = 'free_trial')
-)
+
 select
   user_id,
   Round(avg( case when activity_type = 'free_trial' then activity_duration end),2) as trial_avg_duration,
   Round(avg( case when activity_type = 'paid' then activity_duration end),2) as paid_avg_duration
-from paiduser_cte
-group by user_id;
+from useractivity
+group by user_id having paid_avg_duration is not null and trial_avg_duration is not null
+order by user_id asc;
